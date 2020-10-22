1a67aeedbc8d13893cb30fc9a2f9179c5b25fafe
Eezi <tomi.a.salo@outlook.com>
2020-10-22 20:00:28 +0300

    Reducer and actions has done for products

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,19 @@
+import { 
+    PRODUCT_LIST_FAIL,
+    PRODUCT_LIST_SUCCESS,
+    ç
+} from '../constants/productConstants.js'
+
+export const ProductListReducer = (state = { products: [] }, action) => {
+    switch (action.type) {
+        case PRODUCT_LIST_REQUEST:
+            return { loading: true, products: [] }
+        case PRODUCT_LIST_SUCCESS:
+            return { loading: false, products: action.payload }
+        case PRODUCT_LIST_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state
+             
+    }
+}
