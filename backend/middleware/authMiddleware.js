ae63d2f24dbef04c152e359538efe51249d5894c
Eezi <tomi.a.salo@outlook.com>
2020-11-20 17:27:25 +0200

    Custom authentication middleware done.

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,7 +1,30 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/userModel';
+import asyncHandler from 'express-async-handler';
 
-const protect = async (req, res, next) => {
+const protect = asyncHandler(async (req, res, next) => {
     let token;
-    console.log(req.header.authorization)
-}
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        console.log('Token founded!')
+    }
+
+    try {
+      token = req.headers.authorization.split(' ')[1] 
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      console.log('decoded', decoded);
+      req.user = await User.findById(decoded.id).select('-password')
+      next();
+    } catch (error) {
+        console.log('error', error)
+        res.status(401);
+        throw new Error('Not authorized, token failed')
+    }
+
+    if(!token){
+        res.status(401);
+        throw new Error('Not authorized, no token')
+    }
+
+});
+
+export { protect }
