542d85b04b837d95a235b14096cdc5f4408c795a
Eezi <tomi.a.salo@outlook.com>
2020-12-19 17:28:39 +0200

    Added edit user screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,10 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-import HomeScreen from "../screens/HomeScreen";
+
+import React from 'react'
+import { Route } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import { logout } from "../actions/userActions";
-import { LinkContainer } from "react-router-bootstrap";
-import { NavDropdown } from "react-bootstrap";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -17,102 +16,55 @@ const Header = () => {
     dispatch(logout());
   };
   return (
-    <StyledHeader>
-      <Link to="/">
-        {" "}
-        <H3>WEBSHOP</H3>
-      </Link>
-      <Nav>
-        <Ul>
-          <Link to="/cart">
-            {" "}
-            <Li>
-              <I className="fas fa-shopping-cart"></I>Cart
-            </Li>
-          </Link>
-          {userInfo ? (
-            <NavDropdown title={userInfo.name} id="username">
-              <LinkContainer to="/profile">
-                <NavDropdown.Item>Profile</NavDropdown.Item>
+<header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
               </LinkContainer>
-              <NavDropdown.Item onClick={logoutHandler}>
-                Logout
-              </NavDropdown.Item>
-            </NavDropdown>
-          ) : (
-            <Link to="/login">
-              {" "}
-              <Li>
-                <I className="fas fa-user"></I>Sign In
-              </Li>
-            </Link>
-          )}
-          {userInfo && userInfo.isAdmin && (
-            <NavDropdown title="Admin" id="adminmenu">
-              <LinkContainer to="/admin/userlist">
-                <NavDropdown.Item>Users</NavDropdown.Item>
-              </LinkContainer>
-              <LinkContainer to="/admin/productlist">
-                <NavDropdown.Item>Products</NavDropdown.Item>
-              </LinkContainer>
-              <LinkContainer to="/admin/orderlist">
-                <NavDropdown.Item>Orders</NavDropdown.Item>
-              </LinkContainer>
-            </NavDropdown>
-          )}
-        </Ul>
-      </Nav>
-    </StyledHeader>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <i className='fas fa-user'></i> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title='Admin' id='adminmenu'>
+                  <LinkContainer to='/admin/userlist'>
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to='/admin/productlist'>
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to='/admin/orderlist'>
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 };
 
 export default Header;
-
-const StyledHeader = styled.header`
-  font-family: Arial, Helvetica, sans-serif;
-  top: 0;
-  left: 0;
-  position: fixed;
-  padding: 0.5rem;
-  font-size: 1.2rem;
-  background-color: #303242;
-  color: #fff;
-  margin-bottom: 30px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-
-  width: 100%;
-`;
-
-const I = styled.i`
-  color: #fff;
-`;
-
-const Ul = styled.ul`
-  display: flex;
-  list-style: none;
-
-  margin: 0;
-  color: #fff;
-`;
-
-const Nav = styled.nav`
-  justify-self: right;
-`;
-
-const Li = styled.li`
-  display: inline;
-
-  color: #fff;
-  &:hover {
-    cursor: pointer;
-    text-decoration: underline;
-  }
-`;
-
-const H3 = styled.h3`
-  margin: auto 0 auto 40px;
-  display: block;
-  color: #fff;
-  padding: 0.5rem;
-`;
