6fa626bacf6dccdb6095516fd7031fd239bf2d33
Eezi <tomi.a.salo@outlook.com>
2020-12-22 17:03:17 +0200

    Added endpoints for create and edit product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,41 +1,88 @@
-import asyncHandler from 'express-async-handler';
-import Product from '../models/productModel.js';
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
 
 const getProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find({});
+  const products = await Product.find({});
 
-    res.json(products);
+  res.json(products);
 });
 
 const getProductById = asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
+  const product = await Product.findById(req.params.id);
 
-    if(product) {
-        res.json(product);
-    } else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
-
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
 });
 
-// delete product 
+// delete product
 // Route DELETE /api/products/:id
 // Private/Admin
 const deleteProduct = asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-  
-    if(product){
-      await product.remove();
-      res.json({ message: 'Product removed' })
-    } else {
-      res.status(404);
-      throw new Error('Product not found')
-    }
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product removed" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+// Create new product
+// POST /api/products
+// Access admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    description: "Sample description",
+    price: 0,
+    countInStock: 0,
+    category: "Sample category",
+    brand: "Sample brand",
+    numReviews: 0,
+    image: "./images/sample.jpg",
   });
 
-export {
-    getProducts,
-    getProductById,
-    deleteProduct
-}
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+// Update product
+// PUT /api/products/:id
+// Access admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.status(201).json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export { getProducts, getProductById, deleteProduct, createProduct, updateProduct };
